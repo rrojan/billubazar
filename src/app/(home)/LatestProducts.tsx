f26d3d09f0add23ce8c149ddcb43da3f306f5220
Rojan Rajbhandari <rrojan20@gmail.com>
2024-09-13 16:54:11 +0545

    feat: implement products fetching from db

--- src/app/(home)/LatestProducts.tsx
+++ src/app/(home)/LatestProducts.tsx
@@ -1,35 +1,13 @@
 import ProductCard from "~/components/Cards/ProductCard"
 import { ProductDTO } from "~/types/dto"
 import { SectionHeading } from "./SectionHeading"
+import DBClient from "~/lib/db"
 
-export const LatestProducts = () => {
-  const latestProducts: ProductDTO[] = [
-    {
-      id: "1",
-      title: "Cute sweater",
-      imageUrl: "/images/cat1.jpg",
-      price: 2000,
-    },
-    {
-      id: "2",
-      title: "Hoodie",
-      imageUrl: "/images/cat2.webp",
-      price: 3000,
-    },
-    {
-      id: "3",
-      title: "Cute sweater",
-      imageUrl: "/images/cat1.jpg",
-      price: 2000,
-    },
-    {
-      id: "4",
-      title: "Hoodie",
-      imageUrl: "/images/cat2.webp",
-      price: 3000,
-    },
-  ]
-
+export const LatestProducts = async () => {
+  const db = DBClient.getInstance()
+  const latestProducts = await db.product.findMany({
+    orderBy: { createdAt: "desc" },
+  })
   return (
     <div className="home__latest-products mb-12">
       <SectionHeading>Latest Products</SectionHeading>
@@ -37,7 +15,7 @@ export const LatestProducts = () => {
         {latestProducts.map((product) => (
           <ProductCard
             key={product.id}
-            image={product.imageUrl}
+            image={product.imgUrl}
             title={product.title}
             price={product.price}
           />
--- src/app/(home)/Trending.tsx
+++ src/app/(home)/Trending.tsx
@@ -1,35 +1,13 @@
 import TrendingCard from "~/components/Cards/TrendingCard"
 import { ProductDTO } from "~/types/dto"
 import { SectionHeading } from "./SectionHeading"
+import DBClient from "~/lib/db"
 
-export const Trending = () => {
-  const trendingProducts: ProductDTO[] = [
-    {
-      id: "1",
-      title: "Cute sweater",
-      imageUrl: "/images/cat1.jpg",
-      price: 2000,
-    },
-    {
-      id: "2",
-      title: "Hoodie",
-      imageUrl: "/images/cat2.webp",
-      price: 3000,
-    },
-    {
-      id: "3",
-      title: "Cute sweater",
-      imageUrl: "/images/cat1.jpg",
-      price: 2000,
-    },
-    {
-      id: "4",
-      title: "Hoodie",
-      imageUrl: "/images/cat2.webp",
-      price: 3000,
-    },
-  ]
-
+export const Trending = async () => {
+  const db = DBClient.getInstance()
+  const trendingProducts = await db.product.findMany({
+    orderBy: { createdAt: "asc" },
+  })
   return (
     <div className="home__trending mb-16">
       <SectionHeading>Trending</SectionHeading>
@@ -37,7 +15,7 @@ export const Trending = () => {
         {trendingProducts.map((product) => (
           <TrendingCard
             key={product.id}
-            image={product.imageUrl}
+            image={product.imgUrl}
             title={product.title}
             price={product.price}
           />
